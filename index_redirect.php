9e3814ea2802ca2b63deb28ece8e466f69ea6c42
Andreas Nagel <nagli@naglitech.com>
2019-02-08 20:36:32 -0500

    Initial Upload

---
+++ index_redirect.php
@@ -0,0 +1,21 @@
+<meta http-equiv="refresh" content="0;URL=https://www.pcr-services.net/" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<style type="text/css">
+<!--
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	color: #999;
+}
+-->
+</style>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+REDIRECTING
+</body>
+</html>
